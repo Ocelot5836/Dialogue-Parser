1fc4a7fdb5951c43b08c9350fe28e9dce1931256
Ocelot5836 <FinntheRaider@gmail.com>
2019-07-10 13:54:44 -0700

    Update Test.kt

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,7 +1,7 @@
-import com.ocelot.dialogParser.api.Dialog
-import com.ocelot.dialogParser.api.DialogInfo
-import com.ocelot.dialogParser.api.DialogJsonLoader
-import com.ocelot.dialogParser.api.DialogReader
+import com.ocelot.dialogParser.api.Dialogue
+import com.ocelot.dialogParser.api.DialogueInfo
+import com.ocelot.dialogParser.api.DialogueJsonLoader
+import com.ocelot.dialogParser.api.DialogueReader
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
@@ -22,8 +22,8 @@ fun main() {
     }
 
     val test = Test()
-    val dialogInfo = DialogJsonLoader.load(ClassLoader.getSystemResourceAsStream("test.json"))
-    val dialog = Dialog(dialogInfo, test)
+    val dialogInfo = DialogueJsonLoader.load(ClassLoader.getSystemResourceAsStream("test.json"))
+    val dialog = Dialogue(dialogInfo, test)
     val scanner = Scanner(System.`in`)
     while (!dialog.done) {
         if (dialog.currentText.responses.isEmpty()) {
@@ -34,11 +34,11 @@ fun main() {
     }
 }
 
-class Test : DialogReader {
+class Test : DialogueReader {
 
-    override fun printMessage(dialog: DialogInfo, text: DialogInfo.Text) = println(text.message)
+    override fun printMessage(dialog: DialogueInfo, text: DialogueInfo.Text) = println(text.message)
 
-    override fun printResponse(dialog: DialogInfo, responses: List<DialogInfo.Response>) {
+    override fun printResponse(dialog: DialogueInfo, responses: List<DialogueInfo.Response>) {
         for (response in responses) println(response.message)
         println()
     }
